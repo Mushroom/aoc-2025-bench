1bff56b515aceb57b1faac4a9f2ad1c5fbc56e09
Anthony Roberts <anthony.roberts@linaro.org>
2025-11-26 13:33:51 +0000

    Add comments to template

--- template/main.cpp
+++ template/main.cpp
@@ -8,8 +8,11 @@ int main(int argc, char* argv[])
 #if !NDEBUG
     std::cout << "[DEBUG] Loading file from: " << argv[1] << std::endl;
 #endif
+
+    // Read input file
     std::ifstream inputFileStream(argv[1]);
 
+    // Load input into vector of strings
     std::vector<std::string> input;
     std::string inputLine;
     while(std::getline(inputFileStream, inputLine))
