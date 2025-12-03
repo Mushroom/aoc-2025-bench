9e21301ec30a605d102a854ace7ca2f8c166e2fe
Anthony Roberts <anthony.roberts@linaro.org>
2025-12-03 17:32:25 +0000

    Day 3: Process joltage simultaneously for index

--- 3/main.cpp
+++ 3/main.cpp
@@ -4,12 +4,11 @@
 #include <string>
 #include <filesystem>
 
-__attribute__((always_inline)) inline uint64_t calculatedJoltage(uint8_t* batteries, uint8_t joltageLength)
+__attribute__((always_inline)) inline std::pair<uint64_t, uint64_t> calculatedJoltage(uint8_t* batteries, uint8_t joltageLength1, uint8_t joltageLength2)
 {
     // positions[d][count] = indexes where digit d occurs (0..99)
     uint8_t positions[10][100];
     uint8_t counts[10] = {0};
-    uint8_t idxPointers[10] = {0};
 
     // Loop through row, and note the counts and positions of each digit
     for(uint8_t i = 0; i < 100; ++i)
@@ -18,41 +17,54 @@ __attribute__((always_inline)) inline uint64_t calculatedJoltage(uint8_t* batter
         positions[d][counts[d]++] = i;
     }
 
-    uint64_t total = 0;
-    uint8_t startIndex = 0;
+    // Each of the totals for parts 1 and 2
+    std::pair<uint64_t, uint64_t> totals = {0, 0};
 
-    // Get the initial end index (ie, there must be room for remainder of joltage cells)
-    const uint8_t initialEnd = 101 - joltageLength;
-
-    // Scan through the digits
-    for(uint8_t t = 0; t < joltageLength; ++t)
+    // Process parts 1 and 2 using the same index data for the row
+    for(int i = 0; i < 2; ++i)
     {
-        // Allow for a futher end index based on how many cells have been chosen already
-        uint8_t endIndex = initialEnd + t;
+        uint8_t joltageLength = (i == 0) ? joltageLength1 : joltageLength2;
 
-        // Pick highest digit available between indexes
-        for(int d = 9; d >= 0; --d)
+        uint8_t idxPointers[10] = {0};
+
+        uint64_t total = 0;
+        uint8_t startIndex = 0;
+
+        // Get the initial end index (ie, there must be room for remainder of joltage cells)
+        const uint8_t initialEnd = 101 - joltageLength;
+
+        // Scan through the digits
+        for(uint8_t t = 0; t < joltageLength; ++t)
         {
-            uint8_t p = idxPointers[d];
+            // Allow for a futher end index based on how many cells have been chosen already
+            uint8_t endIndex = initialEnd + t;
 
-            // While pointer within range of counts, and position is before startIndex, advance pointer
-            while(p < counts[d] && positions[d][p] < startIndex) ++p;
-
-            // If pointer within range, and position is before endIndex, we can choose this digit
-            if(p < counts[d] && positions[d][p] < endIndex)
+            // Pick highest digit available between indexes
+            for(int d = 9; d >= 0; --d)
             {
-                total = total * 10 + d;
-                startIndex = positions[d][p] + 1;
-                idxPointers[d] = p + 1;
-                break;
-            }
+                uint8_t p = idxPointers[d];
 
-            // Note where we scanned up to
-            idxPointers[d] = p;
+                // While pointer within range of counts, and position is before startIndex, advance pointer
+                while(p < counts[d] && positions[d][p] < startIndex) ++p;
+
+                // If pointer within range, and position is before endIndex, we can choose this digit
+                if(p < counts[d] && positions[d][p] < endIndex)
+                {
+                    total = total * 10 + d;
+                    startIndex = positions[d][p] + 1;
+                    idxPointers[d] = p + 1;
+                    break;
+                }
+
+                // Note where we scanned up to
+                idxPointers[d] = p;
+            }
         }
+
+        (i == 0) ? totals.first = total : totals.second = total;
     }
 
-    return total;
+    return totals;
 }
 
 int main(int argc, char* argv[])
@@ -88,10 +100,12 @@ int main(int argc, char* argv[])
     {
         uint8_t* batteryRowPtr = batteryArray + (batteryRow * 100);
 
+        auto result = calculatedJoltage(batteryRowPtr, 2, 12);
+
         // Part 1: highest pair
-        totalP1 += calculatedJoltage(batteryRowPtr, 2);
+        totalP1 += result.first;
         // Part 2: highest 12-digit number
-        totalP2 += calculatedJoltage(batteryRowPtr, 12);
+        totalP2 += result.second;
     }
 
     std::cout << totalP1 << std::endl;
